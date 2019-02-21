af4dda5b20fb0f79ff5c4dd6dadc2647e9386ad7
ITSERVICES\tp640d <tp640d@att.com>
2019-02-20 17:01:58 -0800

    Bean Scope

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,24 +2,28 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
-	
+public class SetterDemoApp {
+
 	public static void main(String[] args) {
 		
-		//load the spring configuration file
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		//retrive bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		// retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		//call methos on the bean
+		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
-		// let's call our new method for fortunes
 		System.out.println(theCoach.getDailyFortune());
 		
-		//close the context
+		// call our new methods to get the literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		
+		// close the context
 		context.close();
+
 	}
 
 }
